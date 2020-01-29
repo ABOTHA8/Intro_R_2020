2ca56b8efe38370c74f77a2d591bb2d9c022015e
Amieroh Abrahams <3559244@myuwc.ac.za>
2020-01-29 07:47:55 +0200

    Update Exercise_1.R

--- Exercise_1.R
+++ Exercise_1.R
@@ -16,3 +16,9 @@ laminaria %>%
   group_by(site) %>%
   summarise(max_stipe_mass=max(stipe_mass))
 summarise(min(blade_length))
+
+
+# Very neat scripts
+# Its always good to do more then just that done in class in order to explore the code a bit more
+# Always add comments at the end of each line so that you and the collaborator understands the code
+#Overall mark for day 1: 7/10
